6e9d9292b16d481f9168ae8ea664666028ff2e4f
xudesheng <xudesheng@gmail.com>
2016-10-10 23:22:19 -0400

    Initial Commit

---
+++ Softimage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Softimage
+//
+//  Created by Desheng Xu on 10/10/16.
+//  Copyright © 2016 Desheng Xu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
