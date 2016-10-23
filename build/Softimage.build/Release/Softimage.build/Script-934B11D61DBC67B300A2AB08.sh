9ba3d4b5c01e69578c6ea1edf5782a0a3a805058
xudesheng <xudesheng@gmail.com>
2016-10-23 01:10:08 -0400

    update script
    3 import library can be linked correctly now but one more level library
    can’t be packaged yet.

--- build/Softimage.build/Release/Softimage.build/Script-934B11D61DBC67B300A2AB08.sh
+++ build/Softimage.build/Release/Softimage.build/Script-934B11D61DBC67B300A2AB08.sh
@@ -3,14 +3,19 @@ echo "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
 mkdir "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
 
 export DYLIB=libopencv_core.2.4.13.dylib
+export DYL=libopencv_core.2.4.dylib
 cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
-install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
+cp "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks/$DYL"
+install_name_tool -change /usr/local/opt/opencv/lib/$DYL @loader_path/../Frameworks/$DYL "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
 
 export DYLIB=libopencv_imgproc.2.4.13.dylib
+export DYL=libopencv_imgproc.2.4.dylib
 cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
-install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
+cp "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks/$DYL"
+install_name_tool -change /usr/local/opt/opencv/lib/$DYL @loader_path/../Frameworks/$DYL "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
 
 export DYLIB=libopencv_highgui.2.4.13.dylib
+export DYL=libopencv_highgui.2.4.dylib
 cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
-install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
-
+cp "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks/$DYL"
+install_name_tool -change /usr/local/opt/opencv/lib/$DYL @loader_path/../Frameworks/$DYL "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
